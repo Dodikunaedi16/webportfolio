1048a8cfde4ad975336866e3188c3732a99baf50
MAMANGWARNET <155792168+Dodikunaedi16@users.noreply.github.com>
2024-07-12 09:03:57 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /* typed js*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Teknisi Wifi', 'Blogger'],
+    strings: ['Dodi.Frontend Developer', 'Fitriyah.Apoteker'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
